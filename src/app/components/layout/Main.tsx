31936d8e4d372bdcf2df51a74658ae8ab5abe908
jordlolk <jordeallefsilvadecarvalho@gmail.com>
2025-03-22 00:09:48 -0300

    fix: changing main page and layout

--- src/app/components/layout/Main.tsx
+++ src/app/components/layout/Main.tsx
@@ -1,5 +1,22 @@
+"use client";
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
+
 export default function Main({ children }: { children: React.ReactNode }) {
+  const router = usePathname();
   return (
-    <main className="flex-grow space-y-6 relative text-center">{children}</main>
+    <>
+      <AnimatePresence>
+        <motion.main
+          key={router}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.6 }}
+          className="flex-grow space-y-6 relative text-center"
+        >
+          {children}
+        </motion.main>
+      </AnimatePresence>
+    </>
   );
 }
