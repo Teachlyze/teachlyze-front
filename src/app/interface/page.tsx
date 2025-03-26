b76ebbbcdedad3869851c835dc9be27adf54ee0c
jordlolk <jordeallefsilvadecarvalho@gmail.com>
2025-03-26 00:00:37 -0300

    fix: build a context to watch the user and change page

--- src/app/interface/page.tsx
+++ src/app/interface/page.tsx
@@ -1,21 +1,7 @@
-"use client";
-import { UrlContext } from "@/context/useUrl";
-import { use, useContext, useEffect, useState } from "react";
-
 export default function Interface() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("http://localhost:3000/api/services");
-      const { alunos } = res.ok && (await res.json());
-      setData(alunos);
-    };
-    fetchData();
-  }, []);
   return (
-    <section className="grid grid-cols-1">
-      <h1></h1>
-      <aside className="max-sm:hidden">
+    <section className="grid grid-cols-[auto_1fr] grid-flow-col-dense min-h-screen">
+      <aside className="max-sm:hidden p-4 bg-red-50">
         <nav>
           <ul>
             <li>
@@ -33,18 +19,10 @@ export default function Interface() {
           </ul>
         </nav>
       </aside>
-      <section className="col-span-1 p-4 h-screen bg-cyan-500">
+      <section className="col-span-3 p-4 bg-cyan-500">
         <h2 className="uppercase text-white font-bold font-sans">Alunos</h2>
         <ul className="flex flex-wrap mt-6 justify-between p-2 bg-gray-200">
-          {data &&
-            data.map((aluno: any) => (
-              <li
-                className="p-2 bg-gray-700 text-white w-28 rounded-lg font-bold cursor-pointer"
-                key={aluno.id}
-              >
-                {aluno.nome}
-              </li>
-            ))}
+          <p>EXEMPLO</p>
         </ul>
       </section>
     </section>
--- src/context/useUrl.tsx
+++ src/context/useUrl.tsx
@@ -1,6 +1,20 @@
 "use client";
-import { useRouter } from "next/router";
-import { createContext } from "react";
-let pathName = useRouter();
-export const UrlContext = createContext(pathName);
-console.log(UrlContext);
+import { usePathname } from "next/navigation";
+import { createContext, useContext } from "react";
+type UrlContextType = string;
+
+const UrlContext = createContext<UrlContextType>("");
+export default function UrlProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathName = usePathname();
+  return <UrlContext.Provider value={pathName}>{children}</UrlContext.Provider>;
+}
+
+export function useUrl() {
+  const url = useContext(UrlContext);
+  if (!url) throw new Error("useUrl must be used within a UrlProvider");
+  return url;
+}
